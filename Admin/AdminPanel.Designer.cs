20461a6b0be2833194f102a1191293448c784194
SHOTSUN <k-vlad-k@mail.ru>
2018-04-22 13:34:06 +0300

    adding seller to db from admin

--- Admin/AdminPanel.Designer.cs
+++ Admin/AdminPanel.Designer.cs
@@ -36,8 +36,13 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -47,27 +52,25 @@
             this.tabControl1.Location = new System.Drawing.Point(35, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(322, 199);
+            this.tabControl1.Size = new System.Drawing.Size(351, 328);
             this.tabControl1.TabIndex = 5;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.button1);
-            this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Controls.Add(this.textBox2);
-            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.panel1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(314, 173);
+            this.tabPage1.Size = new System.Drawing.Size(343, 302);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Create Seller";
             this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(64, 106);
+            this.button1.Location = new System.Drawing.Point(29, 142);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 30);
             this.button1.TabIndex = 10;
@@ -78,7 +81,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 72);
+            this.label2.Location = new System.Drawing.Point(13, 68);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 9;
@@ -87,7 +90,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 33);
+            this.label1.Location = new System.Drawing.Point(13, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 8;
@@ -95,14 +98,14 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(86, 69);
+            this.textBox2.Location = new System.Drawing.Point(68, 65);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 7;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(86, 30);
+            this.textBox1.Location = new System.Drawing.Point(68, 26);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 6;
@@ -118,17 +121,60 @@
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(69, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 218);
+            this.panel1.TabIndex = 11;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 19);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "create Seller";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Password";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(69, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            // 
             // AdminPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(438, 261);
+            this.ClientSize = new System.Drawing.Size(438, 377);
             this.Controls.Add(this.tabControl1);
             this.Name = "AdminPanel";
             this.Text = "AdminPanel";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -143,5 +189,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
--- Admin/AdminPanel.cs
+++ Admin/AdminPanel.cs
@@ -16,10 +16,18 @@ namespace Admin
         public AdminPanel()
         {
             InitializeComponent();
+            panel1.Enabled = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if(textBox1.Text == "" || textBox1.Text == "")
+            {
+                MessageBox.Show("Enter valid values !");
+                return;
+             
+            }
+
             string connectionString = @"Server=VLAD-PC\SQLEXPRESS;Database=AIRPORT;Trusted_Connection=True;";
             
             DataSet sellerList = new DataSet("sellerList");
@@ -38,29 +46,56 @@ namespace Admin
             idColumn.AutoIncrementSeed = 0;
             idColumn.AutoIncrementStep = 1;
 
-            DataColumn loginColumn = new DataColumn("name", Type.GetType("System.String"));
-            DataColumn passwordColumn = new DataColumn("surname", Type.GetType("System.String"));
+            DataColumn nameColumn = new DataColumn("name", Type.GetType("System.String"));
+            DataColumn surnameColumn = new DataColumn("surname", Type.GetType("System.String"));
+            DataColumn passwordColumn = new DataColumn("password", Type.GetType("System.String"));
 
             selelrTable.Columns.Add(idColumn);
-            selelrTable.Columns.Add(loginColumn);
+            selelrTable.Columns.Add(nameColumn);
+            selelrTable.Columns.Add(surnameColumn);
             selelrTable.Columns.Add(passwordColumn);
             selelrTable.PrimaryKey = new DataColumn[] { selelrTable.Columns["id"] };
 
-            selelrTable.Rows.Add(new object[] { null, textBox1.Text, textBox2.Text});
+            selelrTable.Rows.Add(new object[] { null, textBox1.Text, textBox2.Text, Manager.GetHashString(textBox3.Text)});
 
             SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+
             try { 
                 adapter.Update(sellerList, "Sellers");
+                MessageBox.Show("Seller " + textBox1.Text + " aded to database.");
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                panel1.Enabled = false;
             }
             catch(System.Data.SqlClient.SqlException exception)
             {
                 MessageBox.Show(exception.Message);
             }
-            }
+
+
+            
+        }
+
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = true;
         }
     }
 }
